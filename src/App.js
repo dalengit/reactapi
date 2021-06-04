3f0a9a0a89698a3f46441222548193a637403306
Dalen Chan <dalen.chan@hotmail.com>
2021-06-04 04:58:02 +0100

    Created Axios request

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [coins, setCoins] = useState([]);
 
-  
+  useEffect( () => {
+    
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+
+    .then(res => {
+      setCoins(res.data);
+    })
+    .catch(error => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>API Project</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input type="text" className="coin-input" placeholder="Search"/>
+        </form>
+      </div>
+      
     </div>
   );
 }
